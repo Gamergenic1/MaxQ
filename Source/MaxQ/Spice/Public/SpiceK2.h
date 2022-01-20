b050ff0bb9bd76fc23fccde5303758f8d8d41c94
gamergenic <82328213+gamergenic@users.noreply.github.com>
2022-01-20 11:24:05 +0800

    Updated source file masthead links

--- Source/MaxQ/Spice/Public/SpiceK2.h
+++ Source/MaxQ/Spice/Public/SpiceK2.h
@@ -1,9 +1,9 @@
 // Copyright 2021 Gamergenic.  See full copyright notice in Spice.h.
 // Author: chucknoble@gamergenic.com|https://www.gamergenic.com
 // 
-// Implementation of:
-// "Adding Third-Party Libraries to Unreal Engine : NASA's SPICE Toolkit"
-// https://gamedevtricks.com/post/third-party-libs-1/
+// Project page:   https://www.gamergenic.com/project/maxq/
+// Documentation:  https://maxq.gamergenic.com/
+// GitHub:         https://github.com/Gamergenic1/MaxQ/ 
 
 #pragma once
 
