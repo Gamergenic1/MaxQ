bdc6e32ac3cbe2eefcf99e03d9cdb69148172f3a
gamergenic <82328213+gamergenic@users.noreply.github.com>
2022-06-09 19:01:09 +0800

    Added very simple keperlian orbit sample (code)

--- Plugins/MaxQ/Source/MaxQCppSamples/Public/Sample05Actor.h
+++ Plugins/MaxQ/Source/MaxQCppSamples/Public/Sample05Actor.h
@@ -19,19 +19,61 @@
 
 /* THIS SAMPLE IS IN PROGRESS */
 
+/* ****************************************************************************
+*  This tutorial is only an interim version.
+*  ***************************************************************************/
+
+// Kepler orbits are paramatized by geometric properties.
+// * Semi-Major axis (or equivalent, such as "perifocal" distance)
+// * Elliptical Eccentricity
+// * 3 DOF Orientation angles (or equivalents)
+//     * Inclination
+//     * Argument of Periapsis
+//     * Longitude of Ascending Node
+// * Epoch (time)
+// * Mean Anomaly at Epoch (or alternative, such as True Anomaly)
+//
+// Kepler orbits are simple to compute but inaccurate.  They're still good for
+// visualizing and orbit once you have a feel for the parameters.
+//
+// Kepler orbit data is useful when dealing with small bodies, like
+// asteroids and comets.  Orbital Elements for nearly any asteroid/comet
+// Known to humans is available from NASA/JPL's Solar System Dynamics group:
+// https://ssd.jpl.nasa.gov/
+
+
+// "Two Line" Element (TLE) data originated from NORAD tracking telemetry.
+// It remains valid for a brief time (a few days)
+// It's more commonly used at least with anything orbiting Earth.
+// Various orbit "propagators" have been used.  SPICE will use "sgp4" here.
+
+// TLE data from Satellite Catalog (SatCat) REST APIs such as space-track.org.
+// For an example of querying this data from Unreal Engine, see:
+// https://gamedevtricks.com/post/call-satcat-rest-api-from-ue-http/
+// https://stupidrockettricks.com/post/call-satcat-rest-api-from-ue-http/
+// https://github.com/gamergenic/ASATDebrisVisualization
+// (Kepler/Conic orbit data is available as well, of course.)
+
+
 UCLASS()
 class MAXQCPPSAMPLES_API ASample05Actor : public AActor
 {
     GENERATED_BODY()
 
 public:
+
     UPROPERTY(EditInstanceOnly, Category = "MaxQ|Samples")
-    FSamplesSolarSystemState SolarSystemState;
+    TArray<FString> BasicKernels;
+
 
 public:
     ASample05Actor();
 
     void BeginPlay() override;
+
+    void conics();
+    void oscelt();
+    void TLEs();
 };
 
 
